cdf72247698e7e22bb85d3fdd673279474c484ab
youngjuning <1003719811@qq.com>
2018-06-03 15:55:30 +0800

    添加画圆角矩形的方法

--- wxPromise.js
+++ wxPromise.js
@@ -236,6 +236,18 @@ const wxPromise = () => {
       })
     })
   }
+  // 画圆角矩形
+  wx.pro.circleImg = (ctx, img, x, y, r) => {
+    ctx.save()
+    var d =2 * r
+    var cx = x + r
+    var cy = y + r
+
+    ctx.arc(cx, cy, r, 0, 2 * Math.PI)
+    ctx.clip()
+    ctx.drawImage(img, x, y, d, d)
+    ctx.restore()
+  }
 }
 
 wxPromise();
