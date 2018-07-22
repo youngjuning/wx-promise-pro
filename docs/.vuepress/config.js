46058eeb7fc8c65dbf473cf018091d7f50a832ff
杨俊宁 <1003719811@qq.com>
2018-07-22 10:21:08 +0800

    配置网站文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: '/wxPromise/',
+  title: 'wxPromise',
+  description: '强大的、优雅的 小程序 Promise 库',
+  themeConfig: {
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/youngjuning/wxPromise' },
+    ]
+  }
+}
