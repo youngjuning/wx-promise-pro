0288a6b4d9fee9c46d462465acf27d52103c0d8a
杨俊宁 <1003719811@qq.com>
2018-08-22 12:23:30 +0800

    测试travis-ci

--- vuepress-deploy.sh
+++ vuepress-deploy.sh
@@ -16,10 +16,17 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# 如果使用 travis 持续集成
+git config --local user.name "杨俊宁"
+git config --local user.email {1003719811@qq.com}
+
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:youngjuning/wxPromise.git master:gh-pages
+# git push -f git@github.com:youngjuning/wxPromise.git master:gh-pages
+
+# 如果使用 travis 持续集成
+git push -f https://${access_token}@github.com/youngjuning/wxPromise.git master:gh-pages
 
 cd -
