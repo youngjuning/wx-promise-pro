7d887ab040476fa87a7b1751ec96bef3a7e4a5af
杨俊宁 <youngjuning@aliyun.com>
2020-03-06 12:11:04 +0800

    配置babel es6 to es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import json from 'rollup-plugin-json'
@@ -9,6 +10,9 @@ const input = 'src/index.js'
 // 插件
 const plugins = [
   json(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
   nodeResolve(), // 告诉 Rollup 如何查找外部模块
   commonjs(), // 将 CommonJS 转换成 ES2015 模块
   terser(), // 压缩文件
